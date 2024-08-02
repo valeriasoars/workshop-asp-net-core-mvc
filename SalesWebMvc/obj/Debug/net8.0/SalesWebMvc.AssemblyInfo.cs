2a20c7f97e225a68a071472dc378e5f168bc6c59
Valeria Soares <valeriasoaressantos2@gmail.com>
2024-08-02 19:24:20 -0300

    Update seller and custom service exception

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4614b5cb21be13db6c72ec642c332fcaa2e1029d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa030fafdb1935483f27c1033713856630b54728")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
