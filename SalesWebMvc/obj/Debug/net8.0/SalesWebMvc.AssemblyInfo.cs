d08a8f7f79266862d69389b2946c19b29f95804e
Valeria Soares <valeriasoaressantos2@gmail.com>
2024-08-04 04:51:57 -0300

    Implementing grouping search

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79f090910e828b070656cfd58f1b7dc441009cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ebb9323eff89fa07595c454b06bcc63cdac5167")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
